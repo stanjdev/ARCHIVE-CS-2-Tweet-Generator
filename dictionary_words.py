9e18c107dbf9187aca2d077bad92704e6de87e95
Stanley Jeong <stanley.jeong@shopify.com>
2022-01-21 17:32:16 -0800

    dictionary_words

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,20 +1,29 @@
 import sys
 import random
 
-number_of_words = int(sys.argv[1:][0])
-
+# random.randint()
 # OR, can randomly select 7 indexes, pull those out of the list and return those
 
-f = open('words.txt', 'r')
-words = f.read()
-list_of_words = words.split(" ")
+def read_file():
+  # f = open('words.txt', 'r')
+  f = open('/usr/share/dict/words', 'r')
+  words = f.readlines()
+  f.close()
+  # list_of_words = words.split(" ")
+  return words
 
-def randomize_file_words():
+def randomize_file_words(list_of_words):
   random.shuffle(list_of_words)
   return list_of_words
 
 if __name__ == '__main__':
-  shuffled_words = randomize_file_words()
-  print(' '.join(shuffled_words[0:number_of_words]))
+  if len(sys.argv) > 1:
+    number_of_words = int(sys.argv[1])
+  else:
+    print('Enter a number!')
+    exit(0)
+  shuffled_words = randomize_file_words(read_file())
+  print(''.join(shuffled_words[0:number_of_words]))
+
 
 
